fefe288c7031ee0f2ce12448569c76ae79226aec
kafisher2988 <37336700+kafisher2988@users.noreply.github.com>
2018-04-24 08:56:58 -0500

    Add files via upload

---
+++ Lunchbot.py
@@ -0,0 +1,12 @@
+import random
+food = ['Daves Downtown', 'Chipotle', 'Naf Naf', 'Greek Grill']
+x = (random.choice(food))
+
+if (x=="Chipotle"):
+    print((x+" Dont forget the guac!"))
+if (x=="Daves Downtown"):
+    print((x+" You know you are going to get the Chicken Tender Wrap"))
+if (x=="Naf Naf"):
+    print((x+" Naf it up"))
+if (x=="Greek Grill"):
+    print((x+" Always get the Gyro and Fries"))
